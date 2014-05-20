1c500198d6a3a1ab91ee6e145beccd1d14ab4120
Laurent <lolo@localhost.localdomain>
2014-05-20 17:53:32 +0200

    Design version flat

--- aside.php
+++ aside.php
@@ -1,13 +1,12 @@
-<aside>
-  <div class="row">
-    <div class="col-md-2">
-      <div data-spy="affix" data-offset-top="0" data-offset-bottom="60">
-        <p><i class="fa fa-phone"></i> 06.22.21.12.42</p>
-        <p><i class="fa fa-home"></i> 18 Place Montesquieu, <br>33650 Brede</p>
+  	<div class="col-lg-2 col-md-2 col-lg-2 col-xs-2">
+      <div class="osteo-nav" data-spy="affix" data-offset-top="190">
+      	<h4>Cabinet</h4>
+        <p>
+          <i class="fa fa-home"></i> 18 Place Montesquieu, <br>33650 Brede<br>
+          <i class="fa fa-phone"></i> 06.22.21.12.42
+        </p>
       </div>
     </div>
-  </div>
-</aside>
 
-</div>
+  </div>
 </div>
